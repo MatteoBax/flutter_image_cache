4c8243fb3a1ee070e81ac521b83aab8f221c031e
MatteoBax <matte.mb2006.9990@gmail.com>
2025-11-06 21:56:17 +0100

    Fixed dependency import

--- lib/src/tools/_tools.dart
+++ lib/src/tools/_tools.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
-import "package:path/path.dart";
 import 'package:path_provider/path_provider.dart';
 import 'package:crypto/crypto.dart';
 
